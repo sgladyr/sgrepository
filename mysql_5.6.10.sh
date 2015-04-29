40fd7901bd90775a667525af62f5e0a40054d06e
sgladyr <serhy.gladyr@gmail.com>
2015-04-29 16:47:25 +0300

    Update mysql_5.6.10.sh

--- mysql_5.6.10.sh
+++ mysql_5.6.10.sh
@@ -1,8 +1,9 @@
 
 Mysql 5.6.10
 http://mervine.net/hacks/installing-mysql-on-centos6
+http://tecadmin.net/install-mysql-5-7-centos-rhel/
 
-rpm -e --nodeps mysql-libs-5.1.71-1.el6.x86_64
+
 
 
 cd /tmp
@@ -10,10 +11,10 @@ wget -O 1.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-shared-5.6.10-1
 wget -O 2.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.10-1.el6.x86_64.rpm/from/http://cdn.mysql.com/
 wget -O 3.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.10-1.el6.x86_64.rpm/from/http://cdn.mysql.com/
 wget -O 4.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-devel-5.6.10-1.el6.i686.rpm/from/http://cdn.mysql.com/
-
+rpm -e --nodeps mysql-libs-5.1.71-1.el6.x86_64
 yum install -y *.rpm
 
-http://tecadmin.net/install-mysql-5-7-centos-rhel/
+
 
 cat /root/.mysql_secret
 mysql -u root -p
@@ -22,3 +23,6 @@ FLUSH PRIVILEGES;
 quit
 
 C:\Users\user\AppData\Roaming\MySQL\Workbench\ssh
+
+/usr/bin/mysql_secure_installation
+mysql -V
