35f4cfee0e68ebe538e56cef2ffeed990afb61b6
sgladyr <serhy.gladyr@gmail.com>
2015-04-29 16:23:03 +0300

    Update mysql_5.6.10.sh

--- mysql_5.6.10.sh
+++ mysql_5.6.10.sh
@@ -1,6 +1,11 @@
 
 Mysql 5.6.10
 http://mervine.net/hacks/installing-mysql-on-centos6
+
+rpm -e --nodeps mysql-libs-5.1.71-1.el6.x86_64
+
+
+cd /tmp
 wget -O 1.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-shared-5.6.10-1.el6.x86_64.rpm/from/http://cdn.mysql.com/
 wget -O 2.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.10-1.el6.x86_64.rpm/from/http://cdn.mysql.com/
 wget -O 3.rpm http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.10-1.el6.x86_64.rpm/from/http://cdn.mysql.com/
