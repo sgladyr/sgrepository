9562d91a36a8c2ac753c76af982c00a3973dbbdc
sgladyr <serhy.gladyr@gmail.com>
2015-06-24 20:32:27 +0300

    Install for JIRA

--- java.sh
+++ java.sh
@@ -1,6 +1,22 @@
 #!/usr/local/bin/bash
-# INSTALL
 
+===================================================================================
+# INSTALL for JIRA
+sudo mkdir /usr/java
+cd /usr/java
+  
+sudo wget http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz
+sudo tar -C /usr/java -xvf jdk-7u45-linux-x64.tar.gz
+sudo ln -s jdk1.7.0_45/ lastest
+sudo ln -s lastest default
+  
+echo 'export JDK_HOME="/usr/java/default"' >> /etc/profile
+echo 'export JAVA_HOME="/usr/java/default"' >> /etc/profile
+echo 'export JRE_HOME="/usr/java/default/jre"' >> /etc/profile
+echo 'PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
+source /etc/profile
+java -version
+=====================================================================================
 
 # ENV
 
