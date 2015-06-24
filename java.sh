de22834d896a470b93c7ffc9b6f98af25c38a248
sgladyr <serhy.gladyr@gmail.com>
2015-06-24 19:57:19 +0300

    Update java.sh

--- java.sh
+++ java.sh
@@ -5,11 +5,19 @@
 # ENV
 
 
-echo $JAVA_HOME
-java -version
-updatedb; locate java | grep bin # (mlocate)
-type -a java
+$echo $JAVA_HOME
+$java -version
+$updatedb; locate java | grep bin # (mlocate)
+$type -a java
 
 # Trobleshooting
 
-echo "DIR ~/src/test created"
+$echo "DIR ~/src/test created"
+
+
+# Firefox
+
+# Для 32 битной системы создаем символическую ссылку, вводим в терминале:
+$sudo ln -s /usr/lib/jvm/jdk1.7.0/jre/lib/amd64/libnpjp2.so /usr/lib/mozilla/plugins
+# Для 64 битной системы создаем символическую ссылку, вводим в терминале:
+$sudo ln -s /usr/lib/jvm/jdk1.7.0/jre/lib/i386/libnpjp2.so /usr/lib/mozilla/plugins 
